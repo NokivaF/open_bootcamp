267097bb904230b45e8ca10916e838a9f5d1144d
kivatrnc <kivatrnc@gmail.com>
2022-07-22 15:31:51 -0500

    Ejercicios tema 3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        App a = new App();
+
+        suma(10, 20, 10);
+
+        Car miCoche = a.new Car();
+        miCoche.getDoors();
+        System.out.println(miCoche.doorsNumber);
+    }
+
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println("El resultado de la suma es: " + resultado);
+
+    }
+
+    class Car {
+        public int doorsNumber = 0;
+
+        public void getDoors() {
+            this.doorsNumber++;
+        }
+
     }
 }
